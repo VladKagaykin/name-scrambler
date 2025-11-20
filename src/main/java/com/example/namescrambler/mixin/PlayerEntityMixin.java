38992e4ec15c46c766de8a5b13224711a31ea522
AVCD <vladkagajkin233@gmail.com>
2025-11-20 21:59:58 +0300

    test hide names

--- src/main/java/com/example/namescrambler/mixin/PlayerEntityMixin.java
+++ src/main/java/com/example/namescrambler/mixin/PlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package com.example.namescrambler.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,20 +10,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
-
+    
+    // Это основное место, где меняется отображаемое имя
     @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
     private void replaceDisplayName(CallbackInfoReturnable<Text> cir) {
         PlayerEntity player = (PlayerEntity) (Object) this;
-        // Скрываем имя над головой через §k
         Text scrambledName = Text.literal("§k" + player.getGameProfile().getName());
         cir.setReturnValue(scrambledName);
     }
-
+    
+    // Дополнительная страховка - меняем имя сущности
     @Inject(method = "getEntityName", at = @At("HEAD"), cancellable = true)
     private void replaceEntityName(CallbackInfoReturnable<String> cir) {
         PlayerEntity player = (PlayerEntity) (Object) this;
-        // Скрываем entity name
         String scrambledName = "§k" + player.getGameProfile().getName();
+        cir.setReturnValue(scrambledName);
+    }
+    
+    // ВАЖНО: Прямое вмешательство в метод получения имени для отображения над головой
+    @Inject(method = "getCustomName", at = @At("HEAD"), cancellable = true)
+    private void replaceCustomName(CallbackInfoReturnable<Text> cir) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        Text scrambledName = Text.literal("§k" + player.getGameProfile().getName());
         cir.setReturnValue(scrambledName);
     }
 }
